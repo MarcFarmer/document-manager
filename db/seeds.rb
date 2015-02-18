879db921d4d93afd44dc8204e8b44c7b5189f340
DarkBlue (Kurogami Aoi) <wudylk@hotmail.com>
2015-02-18 11:09:46 +1030

    basic seeding completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,12 +19,23 @@ User.create!([
    {doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil, email: "2@7.com", password: "12345678", encrypted_password: "$2a$10$XVFk6fpp2/3raSVBYH8GwOIBta.mGbWI0tXsVzFEKSqKRf3nRUx3K", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-17 23:39:48", last_sign_in_at: "2015-02-17 23:39:48", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
    {doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil, email: "2@8.com", password: "12345678", encrypted_password: "$2a$10$XVFk6fpp2/3raSVBYH8GwOIBta.mGbWI0tXsVzFEKSqKRf3nRUx3K", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-17 23:39:48", last_sign_in_at: "2015-02-17 23:39:48", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
    {doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil, email: "2@9.com", password: "12345678", encrypted_password: "$2a$10$XVFk6fpp2/3raSVBYH8GwOIBta.mGbWI0tXsVzFEKSqKRf3nRUx3K", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-17 23:39:48", last_sign_in_at: "2015-02-17 23:39:48", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
-   {doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil, email: "2@10.com", password: "12345678", encrypted_password: "$2a$10$XVFk6fpp2/3raSVBYH8GwOIBta.mGbWI0tXsVzFEKSqKRf3nRUx3K", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-17 23:39:48", last_sign_in_at: "2015-02-17 23:39:48", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"}
+   {doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil, email: "2@10.com", password: "12345678", encrypted_password: "$2a$10$XVFk6fpp2/3raSVBYH8GwOIBta.mGbWI0tXsVzFEKSqKRf3nRUx3K", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-17 23:39:48", last_sign_in_at: "2015-02-17 23:39:48", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
+
+   {doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil, email: "3@1.com", password: "12345678", encrypted_password: "$2a$10$XVFk6fpp2/3raSVBYH8GwOIBta.mGbWI0tXsVzFEKSqKRf3nRUx3K", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-17 23:39:48", last_sign_in_at: "2015-02-17 23:39:48", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
+   {doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil, email: "3@2.com", password: "12345678", encrypted_password: "$2a$10$XVFk6fpp2/3raSVBYH8GwOIBta.mGbWI0tXsVzFEKSqKRf3nRUx3K", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-17 23:39:48", last_sign_in_at: "2015-02-17 23:39:48", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
+   {doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil, email: "3@3.com", password: "12345678", encrypted_password: "$2a$10$XVFk6fpp2/3raSVBYH8GwOIBta.mGbWI0tXsVzFEKSqKRf3nRUx3K", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-17 23:39:48", last_sign_in_at: "2015-02-17 23:39:48", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
+   {doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil, email: "3@4.com", password: "12345678", encrypted_password: "$2a$10$XVFk6fpp2/3raSVBYH8GwOIBta.mGbWI0tXsVzFEKSqKRf3nRUx3K", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-17 23:39:48", last_sign_in_at: "2015-02-17 23:39:48", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
+   {doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil, email: "3@5.com", password: "12345678", encrypted_password: "$2a$10$XVFk6fpp2/3raSVBYH8GwOIBta.mGbWI0tXsVzFEKSqKRf3nRUx3K", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-17 23:39:48", last_sign_in_at: "2015-02-17 23:39:48", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
+   {doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil, email: "3@6.com", password: "12345678", encrypted_password: "$2a$10$XVFk6fpp2/3raSVBYH8GwOIBta.mGbWI0tXsVzFEKSqKRf3nRUx3K", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-17 23:39:48", last_sign_in_at: "2015-02-17 23:39:48", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
+   {doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil, email: "3@7.com", password: "12345678", encrypted_password: "$2a$10$XVFk6fpp2/3raSVBYH8GwOIBta.mGbWI0tXsVzFEKSqKRf3nRUx3K", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-17 23:39:48", last_sign_in_at: "2015-02-17 23:39:48", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
+   {doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil, email: "3@8.com", password: "12345678", encrypted_password: "$2a$10$XVFk6fpp2/3raSVBYH8GwOIBta.mGbWI0tXsVzFEKSqKRf3nRUx3K", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-17 23:39:48", last_sign_in_at: "2015-02-17 23:39:48", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
+   {doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil, email: "3@9.com", password: "12345678", encrypted_password: "$2a$10$XVFk6fpp2/3raSVBYH8GwOIBta.mGbWI0tXsVzFEKSqKRf3nRUx3K", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-17 23:39:48", last_sign_in_at: "2015-02-17 23:39:48", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
+   {doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil, email: "3@10.com", password: "12345678", encrypted_password: "$2a$10$XVFk6fpp2/3raSVBYH8GwOIBta.mGbWI0tXsVzFEKSqKRf3nRUx3K", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-17 23:39:48", last_sign_in_at: "2015-02-17 23:39:48", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"}
 ])
 
 Organisation.create!([
    {name: "Organization-1", user_id: 1},
-   {name: "Organization-2", user_id: 11}
+   {name: "Organization-2", user_id: 11},
 ])
 
 OrganisationUser.create!([
@@ -51,3 +62,59 @@ OrganisationUser.create!([
    {accepted: true, user_type: 1, user_id: 20, organisation_id: 2, inviter_id: 11}
 ])
 
+Document.create!([
+   {title: "doc-1@1's 1", status: 0, user_id: 1, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@1's 2", status: 0, user_id: 1, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@1's 3", status: 0, user_id: 1, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+
+   {title: "doc-1@1's 4", status: 1, user_id: 1, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@1's 5", status: 1, user_id: 1, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@1's 6", status: 1, user_id: 1, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+
+   {title: "doc-1@1's 7", status: 2, user_id: 1, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@1's 8", status: 2, user_id: 1, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@1's 9", status: 2, user_id: 1, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+
+   {title: "doc-1@1's 10", status: 3, user_id: 1, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@1's 11", status: 3, user_id: 1, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@1's 12", status: 3, user_id: 1, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+
+
+
+
+   {title: "doc-1@2's 1", status: 0, user_id: 2, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@2's 2", status: 0, user_id: 2, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@2's 3", status: 0, user_id: 2, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+
+   {title: "doc-1@2's 4", status: 1, user_id: 2, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@2's 5", status: 1, user_id: 2, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@2's 6", status: 1, user_id: 2, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+
+   {title: "doc-1@2's 7", status: 2, user_id: 2, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@2's 8", status: 2, user_id: 2, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@2's 9", status: 2, user_id: 2, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+
+   {title: "doc-1@2's 10", status: 3, user_id: 2, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@2's 11", status: 3, user_id: 2, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@2's 12", status: 3, user_id: 2, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+
+
+
+
+   {title: "doc-1@6's 1", status: 0, user_id: 6, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@6's 2", status: 0, user_id: 6, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@6's 3", status: 0, user_id: 6, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+
+   {title: "doc-1@6's 4", status: 1, user_id: 6, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@6's 5", status: 1, user_id: 6, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@6's 6", status: 1, user_id: 6, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+
+   {title: "doc-1@6's 7", status: 2, user_id: 6, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@6's 8", status: 2, user_id: 6, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@6's 9", status: 2, user_id: 6, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+
+   {title: "doc-1@6's 10", status: 3, user_id: 6, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@6's 11", status: 3, user_id: 6, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+   {title: "doc-1@6's 12", status: 3, user_id: 6, organisation_id: 1, document_type_id: nil, doc_file_name: nil, doc_content_type: nil, doc_file_size: nil, doc_updated_at: nil},
+
+])
