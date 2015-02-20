e459d0eaac55ddaa7215120d3a2a12a5d94a7bf0
thatscool <a1627698@student.adelaide.edu.au>
2015-02-20 11:18:17 +1030

    devise gem overwrite

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     put :save_current_organisation#, :on => :collection
   end
 
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
 
   resources :documents do
@@ -35,6 +35,8 @@ Rails.application.routes.draw do
   match "all_documents" => 'documents#all_documents', via: :post, as: 'all_documents'
 
   root to: "organisations#index"
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
