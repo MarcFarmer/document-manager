0942c46e39defbf7943f96c8d9fdef703d190c16
Marc <a1627644@student.adelaide.edu.au>
2015-02-23 16:47:01 +1030

    New form shows text editor or file upload depending on user selection. Use JQuery.

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -1,4 +1,23 @@
 $(document).ready(function () {
+    if ($('input#document_use_editor_true').prop("checked") == true) {
+        $("div#document_editor_field").show();
+        $("div#document_upload_field").hide();
+    } else if ($('input#document_use_editor_false').prop("checked") == true) {
+        $("div#document_editor_field").hide();
+        $("div#document_upload_field").show();
+    } else {
+        $("div#document_editor_field").hide();
+        $("div#document_upload_field").hide();
+    }
+    $("input#document_use_editor_true").change(function () {
+        $("div#document_editor_field").show();
+        $("div#document_upload_field").hide();
+    });
+    $("input#document_use_editor_false").change(function () {
+        $("div#document_editor_field").hide();
+        $("div#document_upload_field").show();
+    });
+
     $("#document_filter_buttons li").click(function () {
         $(this).addClass("active");
         $(this).siblings("li").each(function () {
