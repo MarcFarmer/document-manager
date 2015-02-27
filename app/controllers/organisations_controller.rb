bb88290e19e278d3e48366ecc56eca5092ac89df
Marc <a1627644@student.adelaide.edu.au>
2015-02-27 16:56:05 +1030

    Remove puts that causes error and commented email related code.

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -76,13 +76,10 @@ class OrganisationsController < ApplicationController
       pending_user.inviter_id = current_user.id
       pending_user.organisation_id = get_current_organisation.id
       pending_user.save
-      #TODO:Send email to new users for org invite
-      #Notifier.org_invite(email,org,inviter)
-      # puts(selected_email,Organisation.find(pending_user.organisation_id).name,User.find(pending_user.inviter_id).email)
-      puts("HELLO" + pending_user.inviter_id)
+
       Notifier.org_invite(selected_email,Organisation.find(pending_user.organisation_id).name,User.find(pending_user.inviter_id).email).deliver_now
 
-      redirect_to :organisations, notice: "Unregistreed user has been invited."
+      redirect_to :organisations, notice: "Unregistered user has been invited."
     else
       # Users that have registered
       if selected_email != ''
@@ -93,11 +90,8 @@ class OrganisationsController < ApplicationController
         invited_user.user_type = params[:organisation_user][:typesSelection].to_i
         invited_user.inviter_id = current_user.id
         invited_user.save
-        #TODO: Send email to users for org invite
-        #Notifier.org_invite(email,org,inviter)
-        # puts(selected_email,Organisation.find(invited_user.organisation_id).name,User.find(invited_user.inviter_id).email)
-        Notifier.org_invite(selected_email,Organisation.find(invited_user.organisation_id).name,User.find(invited_user.inviter_id).email).deliver_now
 
+        Notifier.org_invite(selected_email,Organisation.find(invited_user.organisation_id).name,User.find(invited_user.inviter_id).email).deliver_now
       end
       redirect_to :organisations, notice: "Registreed user has been invited."
     end
